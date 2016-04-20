25c4a29749c47cd49cb8ddd41d7b1751b6c65b7d
paihuangpian <2451678579@qq.com>
2016-04-20 12:02:29 +0800

    '重复下单控制(通过帐号)'

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -15,13 +15,7 @@ class HomeController extends Controller
 
     public function homeOrder(Request $request){
 
-        // 重复下单的后果
-        // $repeat = \DB::select('select * from orders where ip = "' . $_SERVER['REMOTE_ADDR'] . '" and created_at = "' . date('Y-m-d') .  '" and type = ' . $request->input('type'));
-       
-        /*if($repeat){
-            $type = $request->input('type') ? '晚饭' : '午饭';
-            return redirect()->back()->withErrors(['errors' => '施主，你' . $type . '确实已经点了一份，不信，你去问前台妹子，如果非要吃多份，可以主动联系她她她哟~'])->withInput();
-        }*/
+        
 
     	$info = [
             'name.required' => '还没有填写姓名呢~',
@@ -41,6 +35,14 @@ class HomeController extends Controller
             return redirect()->back()->withErrors(['errors' => '最多选择两个菜品'])->withInput();
         }
 
+        // 重复下单的后果
+        $repeat = \DB::select('select * from orders where user_id = "' . \Auth::user()->id . '" and created_at = "' . date('Y-m-d') .  '" and type = ' . $request->input('type'));
+       
+        if($repeat){
+            $type = $request->input('type') ? '晚饭' : '午饭';
+            return redirect()->back()->withErrors(['errors' => '施主，你' . $type . '确实已经点了一份，不信，你去问前台妹子，如果非要吃多份，可以主动联系她她她哟~'])->withInput();
+        }
+        
     	$result = \DB::table('orders')->insert(
 			    ['name' => \Auth::user()->name, 'menus' => implode(',', $request->input('menus')), 'type' => $request->input('type'), 'created_at' => date('Y-m-d'), 'ip' => $_SERVER['REMOTE_ADDR'], 'user_id' => \Auth::user()->id]
 		);
